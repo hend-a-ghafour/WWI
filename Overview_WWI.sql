dfc0970bfe861a0a19eb3e8331cda4c42af77138
Hend Abd El-Ghafour <123007982+hend-a-ghafour@users.noreply.github.com>
2025-07-31 15:44:30 +0000

    Overview - Adding Suppliers & Products

--- Overview_WWI.sql
+++ Overview_WWI.sql
@@ -96,3 +96,83 @@ GROUP BY
 	Position
 ORDER BY
 	Employee_Num DESC;
+
+-- 3- Suppliers
+SELECT	
+	*
+FROM
+	Suppliers;
+
+SELECT	
+	COUNT(*) Suppliers_Count
+FROM
+	Suppliers;
+
+SELECT	
+	Supplier_Category,
+	COUNT(*) Suppliers_Count
+FROM
+	Suppliers
+GROUP BY
+	Supplier_Category
+ORDER BY
+	Suppliers_Count DESC;
+
+-- 4- Products
+SELECT	
+	*
+FROM
+	Products
+	
+	
+SELECT
+	COUNT(*) AS Total_Products,
+	(SELECT 
+		COUNT(Is_Chiller)  
+	FROM 
+		Products 
+	WHERE 
+		Is_Chiller=1) AS Total_Chill_Items,
+	(SELECT 
+		COUNT(Is_Chiller)  
+	FROM 
+		Products 
+	WHERE 
+		Is_Chiller=0) AS Total_Dry_Items,
+	COUNT(DISTINCT Product_Category) AS Categories,
+	CAST(ROUND(AVG(Unit_Cost),2)AS DECIMAL(10,2)) AS Average_Cost,
+	CAST(ROUND(AVG(WholeSale_Price),2)AS DECIMAL(10,2)) AS Average_Wholesale_Price,
+	CAST(ROUND(AVG(Retail_Price),2)AS DECIMAL(10,2)) AS Average_Retail_Price,
+	ROUND(AVG(Lead_Time),2) AS Average_Lead_Time
+FROM
+	Products
+
+
+SELECT
+	Product_Category,
+	COUNT(*) AS Total_Products,
+	SUM(CASE WHEN Is_Chiller =1 THEN 1 END) Total_Chill_Items,
+	SUM(CASE WHEN Is_Chiller =0 THEN 1 END)  AS Total_Dry_Items,
+	CAST(ROUND(AVG(Unit_Cost),2)AS DECIMAL(10,2)) AS Average_Cost,
+	CAST(ROUND(AVG(WholeSale_Price),2)AS DECIMAL(10,2)) AS Average_Wholesale_Price,
+	CAST(ROUND(AVG(Retail_Price),2)AS DECIMAL(10,2)) AS Average_Retail_Price,
+	CAST(ROUND(AVG(Tax_Rate),2)AS DECIMAL(10,2)) AS Average_Tax_Rate,
+	ROUND(AVG(Lead_Time),2) AS Average_Lead_Time
+FROM
+	Products
+GROUP BY
+	Product_Category
+
+
+SELECT
+	P_ID,
+	Q_on_Hand,
+	Target_Stock,
+	Reorder_Level,
+	CASE
+		WHEN Q_on_Hand> Target_Stock THEN 'Above_Target'
+		WHEN Q_on_Hand <= Target_Stock AND Q_on_Hand>Reorder_Level THEN 'Optimal'
+		ELSE 'Restock'
+	END AS Stock_Status
+FROM 
+	Products
